6213acc5eabe439adb32c4be9bbf852ede979168
ufocharles <ufocharles@gmail.com>
2022-04-16 21:54:30 -0500

    added confetti
    added confetti for the congratulations message. for it to work on different screen sizes, i have restricted the body size to 600px

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
  fix Inspiring Apps logo
 
  Button style sourced from https://getcssscan.com/css-buttons-examples
+ confetti was sourced from https://www.youtube.com/watch?v=quSR_ZrVz6Y
 
 The only way to keep the coordinates of the dots the same on every screen size is to make the body the zise of the logo 600px.
--- src/App.js
+++ src/App.js
@@ -1,136 +1,11 @@
-const dragBtns = document.querySelectorAll(".btnDrag");
 
-const dotCoord = {
-    blue: {
-        x: 358,
-        y: 269,
-        upperX: 378,
-        lowerX: 338,
-        uppery: 289,
-        lowery: 249
-    },
-    red: {
-        x: 538,
-        y: 293,
-        upperX: 558,
-        lowerX: 518,
-        uppery: 313,
-        lowery: 273
-    },
-    green: {
-        x: 224,
-        y: 554,
-        upperX: 244,
-        lowerX: 204,
-        uppery: 574,
-        lowery: 534
-    },
-    black: {
-        x: 129,
-        y: 412,
-        upperX: 149,
-        lowerX: 109,
-        uppery: 434,
-        lowery: 392
-    },
-    black1: {
-        x: 501,
-        y: 509,
-        upperX: 521,
-        lowerX: 481,
-        uppery: 529,
-        lowery: 489
-    }
-}
-console.log(dragBtns)
-
-dragBtns.forEach((dragBtn) => {
-    const drag = (e) => {
-        console.log(e)
-        switch (dragBtn.id) {
-            case "black":
-                if ((e.pageX >= dotCoord.black.lowerX && e.pageX <= dotCoord.black.upperX) && (e.pageY >= dotCoord.black.lowery && e.pageY <= dotCoord.black.uppery)) {
-                    console.log('im here 1')
-                    dragBtn.style.top = dotCoord.black.y + "px";
-                    dragBtn.style.left = dotCoord.black.x + "px";
-                } else if ((e.pageX >= dotCoord.black1.lowerX && e.pageX <= dotCoord.black1.upperX) && (e.pageY <= dotCoord.black1.lowery && e.pageY <= dotCoord.black1.uppery)) {
-                    console.log('im here 2')
-                    dragBtn.style.top = dotCoord.black1.y + "px";
-                    dragBtn.style.left = dotCoord.black1.x + "px";
-                } else {
-                    console.log('im here 3')
-                    dragBtn.style.top = "90%";
-                    dragBtn.style.left = "90%";
-                }
-                break;
-            case "black1":
-                if ((e.pageX >= dotCoord.black.lowerX && e.pageX <= dotCoord.black.upperX) && (e.pageY >= dotCoord.black.lowery && e.pageY <= dotCoord.black.uppery)) {
-                    console.log('im here 1')
-                    dragBtn.style.top = dotCoord.black.y + "px";
-                    dragBtn.style.left = dotCoord.black.x + "px";
-                } else if ((e.pageX >= dotCoord.black1.lowerX && e.pageX <= dotCoord.black1.upperX) && (e.pageY <= dotCoord.black1.lowery && e.pageY <= dotCoord.black1.uppery)) {
-                    console.log('im here 2')
-                    dragBtn.style.top = dotCoord.black1.y + "px";
-                    dragBtn.style.left = dotCoord.black1.x + "px";
-                } else {
-                    console.log('im here 3')
-                    dragBtn.style.top = "90%";
-                    dragBtn.style.left = "73%";
-                }
-                break
-            default:
-                break;
-        }
-        dragBtn.style.top = e.pageY + "px";
-        dragBtn.style.left = e.pageX + "px";
-    };
-
-    dragBtn.addEventListener("mousedown", () => {
-        window.addEventListener("mousemove", drag);
-    });
-
-    window.addEventListener("mouseup", () => {
-        window.removeEventListener("mousemove", drag)
-    });
-});
-
-function mouseRelease(e, dragBtn) {
-    console.log(e)
-    dragBtn.style.top = e.pageY + "px";
-    dragBtn.style.left = e.pageX + "px";
-}
+let dotCoord = []
 
 // get reset button
 const resetButton = document.getElementById("reset")
 resetButton.addEventListener('click', () => {
     console.log('reset me')
     location.reload()
-    // dragBtns.forEach((dragBtn) => {
-    //     switch (dragBtn.id) {
-    //         case 'black':
-    //             dragBtn.style.top = "90%";
-    //             dragBtn.style.left = "90%";
-    //             break
-    //         case 'black1':
-    //             dragBtn.style.top = "90%";
-    //             dragBtn.style.left = "73%";
-    //             break
-    //         case 'blue':
-    //             dragBtn.style.top = "90%";
-    //             dragBtn.style.left = "15%";
-    //             break
-    //         case 'green':
-    //             dragBtn.style.top = "90%";
-    //             dragBtn.style.left = "55%";
-    //             break
-    //         case 'red':
-    //             dragBtn.style.top = "90%";
-    //             dragBtn.style.left = "35%";
-    //             break
-    //         default:
-    //             break;
-    //     }
-    // });
 });
 
 function allowDrop(ev) {
@@ -149,41 +24,126 @@ function drop(ev) {
     console.log(data)
     switch (data) {
         case 'black':
-            if (ev.target.id === 'blackDiv') {
-                ev.target.appendChild(document.getElementById(data));
+            // check if black exist already in the 2 black divs
+            const black1Exist = checkDotExist('black1')
+            console.log({ black1Exist })
+            if (!black1Exist) {
+                if (ev.target.id === 'blackDiv' || ev.target.id === 'black1Div') {
+                    ev.target.appendChild(document.getElementById(data));
+                    dotCoord.push({
+                        dotName: 'black',
+                        dotDiv: ev.target.id
+                    })
+                    completeLogo()
+                }
             } else {
-                null
+                // figure out the other black spot available
+                const currentBlack1 = dotCoord.find(dot => { return dot.dotName === 'black1' })
+                if (currentBlack1.dotDiv === 'black1Div') {
+                    if (ev.target.id === 'blackDiv') {
+                        ev.target.appendChild(document.getElementById(data));
+                        dotCoord.push({
+                            dotName: 'black',
+                            dotDiv: ev.target.id
+                        })
+                        completeLogo()
+                    }
+                } else {
+                    if (ev.target.id === 'black1Div') {
+                        ev.target.appendChild(document.getElementById(data));
+                        dotCoord.push({
+                            dotName: 'black',
+                            dotDiv: ev.target.id
+                        })
+                        completeLogo()
+                    }
+                }
             }
             break
         case 'black1':
-            if (ev.target.id === 'black1Div') {
-                ev.target.appendChild(document.getElementById(data));
+            const blackExist = checkDotExist('black')
+            console.log({ blackExist })
+
+            if (!blackExist) {
+                if (ev.target.id === 'blackDiv' || ev.target.id === 'black1Div') {
+                    ev.target.appendChild(document.getElementById(data));
+                    dotCoord.push({
+                        dotName: 'black1',
+                        dotDiv: ev.target.id
+                    })
+                    completeLogo()
+                }
             } else {
-                null
+                const currentBlack = dotCoord.find(dot => { return dot.dotName === 'black' })
+                if (currentBlack.dotDiv === 'black1Div') {
+                    if (ev.target.id === 'blackDiv') {
+                        ev.target.appendChild(document.getElementById(data));
+                        dotCoord.push({
+                            dotName: 'black',
+                            dotDiv: ev.target.id
+                        })
+                        completeLogo()
+                    }
+                } else {
+                    if (ev.target.id === 'black1Div') {
+                        ev.target.appendChild(document.getElementById(data));
+                        dotCoord.push({
+                            dotName: 'black',
+                            dotDiv: ev.target.id
+                        })
+                    }
+                }
             }
             break
         case 'blue':
             if (ev.target.id === 'blueDiv') {
                 ev.target.appendChild(document.getElementById(data));
-            } else {
-                null
+                dotCoord.push({
+                    dotName: 'blue',
+                    dotDiv: ev.target.id
+                })
+                completeLogo()
             }
             break
         case 'green':
             if (ev.target.id === 'greenDiv') {
                 ev.target.appendChild(document.getElementById(data));
-            } else {
-                null
+                dotCoord.push({
+                    dotName: 'green',
+                    dotDiv: ev.target.id
+                })
             }
             break
         case 'red':
             if (ev.target.id === 'redDiv') {
                 ev.target.appendChild(document.getElementById(data));
-            } else {
-                null
+                dotCoord.push({
+                    dotName: 'red',
+                    dotDiv: ev.target.id
+                })
+                completeLogo()
             }
             break
         default:
             break;
+    }
+}
+
+
+function checkDotExist(color) {
+    if (dotCoord.find(dot => { return dot.dotName === color })) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// check is all dats are placed correctly
+function completeLogo() {
+    console.log('im confetti')
+    if (dotCoord.length === 5) {
+        const congratDiv = document.getElementById('dotComplete')
+        congratDiv.style.display = "block"
+        confetti.start()
     }
 }
